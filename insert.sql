6ad148205c60693bdab895516f9e6a8d79b243e9
habibabdo <habibdesigner9971728@gmail.com>
2020-06-22 23:12:04 +0300

    insert file created

---
+++ insert.sql
@@ -0,0 +1,7 @@
+import mysql
+
+insert into  flights (origin,destination,duration) values ('New York','London',415);
+insert into  flights (origin,destination,duration) values ('Shanghai','Paris',415);
+insert into  flights (origin,destination,duration) values ('Istanbol','Tokyo',415);
+insert into  flights (origin,destination,duration) values ('Moscow','pARIS',415);
+insert into  flights (origin,destination,duration) values ('Lima','New York',415);
