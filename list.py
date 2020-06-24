12bfa284425324b17ebedf843d33795a53fc848c
habibabdo <habibdesigner9971728@gmail.com>
2020-06-24 20:23:12 +0300

    list.py changed

--- list.py
+++ list.py
@@ -1,4 +1,17 @@
 import os
 
 from sqlalchemy import  create_engine
-from sqlalchemy.orm import scoped_session ,sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine=create_engine(os.getenv('DATABASE_URL'))
+
+db=scoped_session(sessionmaker(bind=engine))
+
+def main():
+    flights =db.executr("SELECT origin, destination,duration FROM flights").fetchall()
+    for flight in flights:
+        print(f"{ flight.origin } to { flight.destination },{ flight.duration } minuts."'")
+
+
+if __name__== '__main':
+    main()
