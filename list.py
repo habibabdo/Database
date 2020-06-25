5b0e29869cdbbc41275cbdc5e3f545f81b4eea9c
habibabdo <habibdesigner9971728@gmail.com>
2020-06-25 14:48:45 +0300

    list.py changed

--- list.py
+++ list.py
@@ -1,17 +1,24 @@
+
 import os
 
-from sqlalchemy import  create_engine
+from Flask import flask
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine=create_engine(os.getenv('DATABASE_URL'))
+# app.config['DATABASE_URL'] = "C:/Users/HD997/Database/"
+# DATABASE_URL="postgres://localhost:5432/database"
 
-db=scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv('//root:1234@ip:80, 443/salon '))
+
+db = scoped_session(sessionmaker(bind=engine))
+
 
 def main():
-    flights =db.executr("SELECT origin, destination,duration FROM flights").fetchall()
+    flights = db.executr('SELECT origin, destination,duration FROM flights').fetchall()
     for flight in flights:
-        print(f"{ flight.origin } to { flight.destination },{ flight.duration } minuts."'")
+        print(f"{flight.origin} to {flight.distination} ,{flight.duration}  minutes.")
 
 
-if __name__== '__main':
+if __name__ == '__main':
     main()
